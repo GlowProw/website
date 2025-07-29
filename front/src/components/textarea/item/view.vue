f75dee0b48a7551cc5b451cd371f19c63f61b9f0
Cabbagelol <nickmiku@foxmail.com>
2025-07-29 11:28:51 +0800

    v0.0.1+7

--- front/src/components/textarea/item/view.vue
+++ front/src/components/textarea/item/view.vue
@@ -1,9 +1,10 @@
 <script setup lang="ts">
 import {nodeViewProps, NodeViewWrapper} from "@tiptap/vue-3";
-import ItemIconWidget from "../../snbWidget/ItemIconWidget.vue";
-import ItemSlotBase from "../../snbWidget/ItemSlotBase.vue";
 import {useI18n} from "vue-i18n";
 import {useI18nUtils} from "../../../assets/sripts/i18nUtil";
+
+import ItemSlotBase from "../../snbWidget/ItemSlotBase.vue";
+import ItemIconWidget from "../../snbWidget/itemIconWidget.vue";
 
 const props = defineProps(nodeViewProps);
 const {t} = useI18n(),
