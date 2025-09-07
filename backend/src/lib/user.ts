67c7582d3e48d52c42838cea0ab78367962f07c1
Cabbagelol <nickmiku@foxmail.com>
2025-09-07 15:07:59 +0800

    v0.0.1+13+1

--- backend/src/lib/user.ts
+++ backend/src/lib/user.ts
@@ -27,7 +27,41 @@ const userAttributes: any = {
     PASSWORD_MIN_LENGTH = 6,
     PASSWORD_MAX_LENGTH = 60,
     ILLEGAL_USERNAMES: ReadonlySet<string> = new Set([
-        "null", "undefined", " ", "\t", "\n"
+        // JavaScript 关键字和保留字
+        "null", "undefined", "true", "false", "this", "super", "new", "function", "var", "let", "const",
+        "if", "else", "for", "while", "do", "switch", "case", "default", "break", "continue", "return",
+        "try", "catch", "finally", "throw", "typeof", "instanceof", "void", "delete", "in", "with",
+        "class", "extends", "import", "export", "await", "async", "yield", "debugger", "eval", "arguments",
+
+        // 全局对象和函数
+        "window", "document", "console", "alert", "confirm", "prompt", "setTimeout", "setInterval",
+        "clearTimeout", "clearInterval", "localStorage", "sessionStorage", "cookie", "navigator",
+        "screen", "history", "location", "performance", "XMLHttpRequest", "fetch", "Promise",
+        "Array", "Object", "String", "Number", "Boolean", "Date", "Math", "RegExp", "JSON",
+        "Error", "TypeError", "ReferenceError", "SyntaxError", "Map", "Set", "WeakMap", "WeakSet",
+
+        // HTML 相关
+        "div", "span", "body", "head", "html", "script", "style", "link", "meta", "img", "a", "form",
+        "input", "button", "table", "tr", "td", "th", "ul", "ol", "li", "select", "option",
+
+        // 常见框架和库的全局变量
+        "$", "jQuery", "Vue", "React", "Angular", "axios", "lodash", "_", "moment", "dayjs",
+
+        // 特殊值和空值
+        "", " ", "\t", "\n", "\r",
+
+        // 系统相关
+        "admin", "root", "system", "user", "guest", "test", "demo",
+
+        // 其他常见保留名称
+        "index", "main", "app", "application", "config", "settings", "options", "data", "value",
+        "name", "id", "key", "password", "email", "phone", "userid", "username", "token",
+        "api", "endpoint", "url", "uri", "path", "route", "method", "get", "post", "put", "delete",
+        "server", "client", "database", "db", "query", "result", "error", "success", "status",
+        "version", "build", "release", "debug", "production", "development", "test", "staging",
+
+        // 保留名称
+        "admin", "root", "official", "glow-prow", "glow_prow", "glow prow", "glow prow admin"
     ] as const);
 
 function userShowAttributes(attr: any, showPrivate = false, force = false) {
