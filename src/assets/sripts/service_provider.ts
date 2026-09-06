f9ba036b834584dcfe97ab7abe06bb91289c8cd1
Cabbagelol <nickmiku@foxmail.com>
2026-09-06 08:48:05 +0800

    v0.0.7+5

--- src/assets/sripts/service_provider.ts
+++ src/assets/sripts/service_provider.ts
@@ -1,4 +1,7 @@
-interface ServiceProviderItem {
+export type ServiceProviderType = 'openSource' | 'techSupport'
+
+export interface ServiceProviderItem {
+    type: ServiceProviderType | string
     name: string,
     icon: string,
     src: string
@@ -7,29 +10,40 @@ interface ServiceProviderItem {
 export class Service_provider {
     private static list: ServiceProviderItem[] = [
         {
+            type: 'openSource',
             name: 'edgeone',
             icon: 'edgeone.png',
             src: 'https://edgeone.ai'
         },
         {
+            type: 'openSource',
             name: 'cloudflare',
             icon: 'cloudflare.svg',
             src: 'https://cloudflare.com'
         },
         {
+            type: 'openSource',
             name: 'cloudflare_pages',
             icon: 'cloudflare_pages.svg',
             src: 'https://pages.cloudflare.com'
         },
         {
+            type: 'openSource',
             name: 'crowdin',
             icon: 'crowdin.png',
             src: 'https://crowdin.com/?utm_term=click-badge-add-on'
         },
         {
+            type: 'openSource',
             name: 'datadoghq',
             icon: 'datadoghq.png',
             src: 'https://www.datadoghq.com'
+        },
+        {
+            type: 'techSupport',
+            name: 'ubisoft',
+            icon: 'ubisoft.svg',
+            src: 'https://www.ubisoft.com/en-us/company/careers/locations/singapore'
         },
     ]
 
--- src/components/ServiceProviderWidget.vue
+++ src/components/ServiceProviderWidget.vue
@@ -3,16 +3,17 @@ export default { name: 'ServiceProviderWidget' }
 </script>
 
 <script setup lang="ts">
-
-import {Service_provider} from "@/assets/sripts/service_provider";
+import {Service_provider, type ServiceProviderItem} from "@/assets/sripts/service_provider";
 import {useAssetsStore} from "~/stores/assetsStore";
-import {onMounted, ref} from "vue";
+import {onMounted, ref, computed} from "vue";
+import {useI18n} from "vue-i18n";
 
+const {t} = useI18n()
 
 const serviceProviders = new Service_provider().services,
     {serializationMap} = useAssetsStore()
 
-let service_providers = ref([])
+const service_providers = ref<ServiceProviderItem[]>([])
 
 onMounted(() => {
   const logosMap = import.meta.glob('/src/assets/images/logos/*.*', {eager: true}),
@@ -24,25 +25,73 @@ onMounted(() => {
     }
   })
 })
+
+const groups = computed(() => {
+  const groupOrder = ['openSource', 'techSupport']
+  const map: Record<string, ServiceProviderItem[]> = {}
+
+  service_providers.value.forEach(item => {
+    if (!map[item.type]) {
+      map[item.type] = []
+    }
+    map[item.type].push(item)
+  })
+
+  const orderedTypes = [
+    ...groupOrder.filter(type => map[type]?.length),
+    ...Object.keys(map).filter(type => !groupOrder.includes(type) && map[type]?.length)
+  ]
+
+  return orderedTypes.map(type => ({
+    type,
+    list: map[type]
+  }))
+})
 </script>
 
 <template>
-  <div class="service-provider d-flex ga-10 overflow-x-auto">
-    <a :href="i.src" target="_blank" v-for="(i, index) in service_providers" :key="index">
-      <img :alt="i.name" :src="i.icon" height="30px"/>
-    </a>
+  <div class="service-provider d-flex align-center flex-nowrap ga-8">
+    <template v-for="(group, gIndex) in groups" :key="group.type">
+      <div class="service-group d-flex align-center flex-nowrap ga-5">
+        <span class="service-label text-no-wrap text-caption font-weight-medium">
+          {{ t(`footer.serviceProvider.${group.type}`) }}
+        </span>
+        <div class="service-items d-flex align-center flex-nowrap ga-8">
+          <a :href="i.src" target="_blank" v-for="(i, index) in group.list" :key="index" :title="i.name">
+            <img :alt="i.name" :src="i.icon" height="30px"/>
+          </a>
+        </div>
+      </div>
+      <v-divider
+          v-if="gIndex < groups.length - 1"
+          vertical
+          class="mx-2 align-self-center opacity-20"
+          style="height: 24px;"
+      ></v-divider>
+    </template>
   </div>
 </template>
 
 <style scoped lang="less">
 .service-provider {
-  a {
-    opacity: .7;
-    animation: all .25s;
+  .service-label {
+    opacity: .6;
+    letter-spacing: .5px;
+    user-select: none;
   }
 
-  a:hover {
-    opacity: 1;
+  .service-items {
+    a {
+      opacity: .7;
+      display: inline-flex;
+      align-items: center;
+      transition: opacity .25s ease, transform .25s ease;
+
+      &:hover {
+        opacity: 1;
+        transform: translateY(-1px);
+      }
+    }
   }
 }
 </style>
